e88474cb11d29feb52a608e4f47d2149ef32dd65
Gleb Erofteev <erofteev.g@toredo.ru>
2023-09-05 15:55:40 +0300

    add prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css/");
 
   return {
+    pathPrefix: '11ty-data',
     dir: {
       input: "src",
       output: "public",
